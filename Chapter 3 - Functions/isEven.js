9eec3b7d4da5014fe51e16ff4bd34f46be0be381
Ryan Roat <ryanroat@gmail.com>
2020-07-04 12:33:11 -0400

    adding chapter 3 exercises

---
+++ Chapter 3 - Functions/isEven.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-else-return */
+function isEven(num) {
+    // console.log(num);
+    if (num == 0) {
+        return true;
+    } else if (num == 1) {
+        return false;
+    } else return isEven(num - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+
+// console.log(isEven(-1));
---
+++ Chapter 3 - Functions/min.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+function min(val1, val2) {
+    return val1 < val2 ? val1 : val2;
+}
+
+console.log(min(1, 2));
+console.log(min(3, 2));
+console.log(min(3, 3));
