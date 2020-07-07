c35f368b21d3e8b79da6dc69777b057452b9a594
Ryan Roat <ryanroat@gmail.com>
2020-07-06 21:14:31 -0400

    sum function

--- Chapter 4 - Data Structures/sum.js
+++ Chapter 4 - Data Structures/sum.js
@@ -9,4 +9,14 @@ function range(start, end) {
     return array;
 }
 
+function sum(numArr) {
+    const l = numArr.length;
+    let total = 0;
+    for (i = 0; i < l; i++) {
+        total += numArr[i];
+    }
+    return total;
+}
+
 console.log(range(5, 25));
+console.log(sum(range(1, 10)));
